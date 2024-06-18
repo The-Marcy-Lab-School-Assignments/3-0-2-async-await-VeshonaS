45300871f47eade030586ebcc3ea1da839690c80
VeshonaS <aniahshell1@gmail.com>
2024-06-17 23:34:54 -0400

    almost there

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,16 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url , options = {}) => {
+     
+     try{
+      const res = await fetch(url, options)
+      if (!res.ok) throw new Error('Fetch failed with status - '[STATUS], [STATUS_TEXT])
+        const isJson = (response.headers.get('content-type') || '').includes('application/json');
+      if(isJson) res.text('')
+     } 
+    
+     catch(error) {
+      
+        console.warn(error)
+        return [null, error ]
+     }
+    return [{}, null]
 };
