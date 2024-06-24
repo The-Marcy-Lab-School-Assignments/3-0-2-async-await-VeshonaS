10142b1f65ef8bf1c3b1d3fafe13fdb081de493c
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-23 23:25:58 -0400

    feedback

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -4,6 +4,7 @@ export const fetchHandler = async (url , options = {}) => {
       const res = await fetch(url, options)
       if (!res.ok) throw new Error('Fetch failed with status - '[STATUS], [STATUS_TEXT])
         const isJson = (response.headers.get('content-type') || '').includes('application/json');
+      /** FEEDBACK: You are so close! After checking if the content type is json, you should return it in json formatting however if it is not, you should return it in text formatting! */
       if(isJson) res.text('')
      } 
     
